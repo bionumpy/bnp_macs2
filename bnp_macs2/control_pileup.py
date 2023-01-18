a48661919924bfd30ca902f807e506dc967d3e4a
Knut Rand <knutdrand@gmail.com>
2023-01-18 11:12:55 +0100

    remove print, correct log

--- bnp_macs2/control_pileup.py
+++ bnp_macs2/control_pileup.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 def get_average_pileup(reads: Bed6, window_size: int, geometry: Geometry):
-    logging.info("Getting fragment pileup")
+    logging.info(f"Getting average pileup for window_size {window_size}")
     windows = dataclasses.replace(reads, 
                                   start=reads.start-window_size//2,
                                   stop=reads.start+window_size//2)
@@ -19,6 +19,5 @@ def get_control_pileup(reads, window_sizes, read_rate, geometry):
     pileup = float(read_rate)
     for window_size in window_sizes:
         avg_pileup = get_average_pileup(reads, window_size, geometry)
-        print(avg_pileup._global_track)
         pileup = np.maximum(pileup, avg_pileup)
     return pileup
